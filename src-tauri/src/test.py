d9354a912ade16e7f9a6070bb9c07cf31c09f789
Nick Conway <nick@conway.dev>
2022-10-01 13:40:33 -0400

    test commit

---
+++ src-tauri/src/test.py
@@ -0,0 +1,12 @@
+print("aaaaaa")
+print("aaaaaa")
+print("aaaaaa")
+print("aaaaaa")
+print("aaaaaa")
+print("aaaaaa")
+print("aaaaaa")
+
+
+f = open("../tmp/demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
