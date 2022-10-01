e47c75f4dfdbbb70616033b2b2c431c6ca36d320
Nick Conway <nick@conway.dev>
2022-10-01 11:33:49 -0400

    Add icon files

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,9 +9,21 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use tauri::api::dialog;
+#[tauri::command]
+async fn choose_file() -> String {
+    let file = dialog::blocking::FileDialogBuilder::new().pick_file();
+    let path = match file {
+        Some(p) => p.into_os_string().into_string().unwrap(),
+        None => "".into()
+    };
+    return path;
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, choose_file])
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,16 +1,35 @@
 const { invoke } = window.__TAURI__.tauri;
 
-let greetInputEl;
-let greetMsgEl;
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-});
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+async function choose_file_encryption() {
+    document.querySelector("#encryption-msg").textContent = await invoke("choose_file", {});
 }
 
-window.greet = greet;
+async function choose_file_decryption() {
+    document.querySelector("#decryption-msg").textContent = await invoke("choose_file", {});
+}
+
+async function toggle_type() {
+
+    let encryption_display = getComputedStyle(document.querySelector('div.encryption'), {}).display;
+    let decryption_display = getComputedStyle(document.querySelector('div.decryption'), {}).display;
+
+    console.log(encryption_display);
+    console.log(decryption_display);
+
+    if (encryption_display === "flex") {
+        document.querySelector('span.encryption').classList.remove("selected");
+        document.querySelector('div.encryption').style.display = "none";
+        document.querySelector('span.decryption').classList.add("selected");
+        document.querySelector('div.decryption').style.display = "flex";
+    } else if (decryption_display === "flex") {
+        document.querySelector('span.decryption').classList.remove("selected");
+        document.querySelector('div.decryption').style.display = "none";
+        document.querySelector('span.encryption').classList.add("selected");
+        document.querySelector('div.encryption').style.display = "flex";
+    }
+
+}
+
+window.choose_file_encryption = choose_file_encryption;
+window.choose_file_decryption = choose_file_decryption;
+window.toggle_type = toggle_type;
