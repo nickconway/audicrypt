63ec942307005efff38a314c6f12d66f7a07e38a
Nick Conway <nick@conway.dev>
2022-10-01 13:40:28 -0400

    test commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,36 @@
 const { invoke } = window.__TAURI__.tauri;
+const { Command } = window.__TAURI__.shell
+
+let encryption_file;
+let decryption_file;
 
 async function choose_file_encryption() {
-    document.querySelector("#encryption-msg").textContent = await invoke("choose_file", {});
+    encryption_file = await invoke("choose_file", {});
+    document.querySelector("#encryption-msg").textContent = encryption_file;
 }
 
 async function choose_file_decryption() {
-    document.querySelector("#decryption-msg").textContent = await invoke("choose_file", {});
+    decryption_file = await invoke("choose_file", {});
+    document.querySelector("#decryption-msg").textContent = decryption_file;
+}
+
+async function encrypt() {
+    console.log("encrypting");
+    const command = new Command("run-py", ["-e", "text", "test.mp3"]);
+    command.on('close', data => {
+      console.log(`command finished with code ${data.code} and signal ${data.signal}`)
+    });
+    command.on('error', error => console.error(`command error: "${error}"`));
+    command.stdout.on('data', line => console.log(`command stdout: "${line}"`));
+    command.stderr.on('data', line => console.log(`command stderr: "${line}"`));
+
+    const child = await command.spawn();
+    console.log('pid:', child.pid);
+    await invoke ("encrypt", {});
+}
+
+async function decrypt() {
+    await invoke ("decrypt", {});
 }
 
 async function toggle_type() {
@@ -32,4 +57,6 @@ async function toggle_type() {
 
 window.choose_file_encryption = choose_file_encryption;
 window.choose_file_decryption = choose_file_decryption;
+window.encrypt = encrypt;
+window.decrypt = decrypt;
 window.toggle_type = toggle_type;
